fdbbda66d44d147ee94c41272161ac3f1c342224
Manoj Reddy <luckymanoj1116@gmail.com>
2024-08-06 19:14:41 -0400

    Made changes to Quiz Interface for question Id's

--- src/main/java/com/example/quizservice/service/QuizService.java
+++ src/main/java/com/example/quizservice/service/QuizService.java
@@ -38,16 +38,10 @@ public class QuizService {
     }
 
     public ResponseEntity<List<QuestionWrapper>> getQuizQuestions(Integer id) {
-//        Optional<Quiz> quiz = quizDao.findById(id);
-//        List<Question> questionsFromDB = quiz.get().getQuestions();
-       List<QuestionWrapper> questionsForUser = new ArrayList<>();
-//        for(Question q : questionsFromDB){
-//            QuestionWrapper qw = new QuestionWrapper(q.getId(), q.getQuestionTitle(), q.getOption1(), q.getOption2(), q.getOption3(), q.getOption4());
-//            questionsForUser.add(qw);
-//        }
-
-        return new ResponseEntity<>(questionsForUser, HttpStatus.OK);
-
+       Quiz quiz = quizDao.findById(id).get();
+       List<Integer> questionIds = quiz.getQuestionIds();
+       ResponseEntity<List<QuestionWrapper>> questions = quizInterface.getQuestionsFromId(questionIds);
+       return questions;
     }
 
     public ResponseEntity<Integer> calculateResult(Integer id, List<Response> responses) {
